8e64ffc02c7f8deee64b17741c4dcbd65f6d802c
md2eoseo <md2eoseo@gmail.com>
2020-03-30 12:51:18 +0200

    getting condition names and added icons, styling

---
+++ Weather.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+export default function Weather({ temp }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          name="weather-lightning-rainy"
+          size={106}
+          // color="white"
+        />
+        <Text style={styles.temp}>{temp}℃</Text>
+      </View>
+      <View style={styles.halfContainer}>
+        <Text>hihi</Text>
+      </View>
+    </View>
+  );
+}
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Clear",
+    "Clouds",
+    "Mist",
+    "Smoke",
+    "Haze",
+    "Dust",
+    "Fog",
+    "Sand",
+    "Ash",
+    "Squall",
+    "Tornado"
+  ]).isRequired
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  temp: {
+    fontSize: 40
+  }
+});
